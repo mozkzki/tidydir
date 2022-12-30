d1378e44cccdf785e9b8745599335661ce0290d2
mozkzki <mozkzki@gmail.com>
2022-12-30 15:58:35 +0900

    HEIC追加

--- src/tidydir/define.py
+++ src/tidydir/define.py
@@ -1,4 +1,4 @@
-TARGET_EXTENSIONS_IMAGE = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
+TARGET_EXTENSIONS_IMAGE = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG", "heic", "HEIC"]
 TARGET_EXTENSIONS_MOVIE = [".mp4", ".MP4", ".mov", ".MOV", ".mts", ".MTS"]
 TARGET_EXTENSIONS = []
 TARGET_EXTENSIONS.extend(TARGET_EXTENSIONS_IMAGE)
